ac3ef6354e53feca7596d5a04d11e3f97b151af0
Liadov Nicolay <avtodrivevid@gmail.com>
2015-12-26 18:11:51 +0200

    TestClientConsole

---
+++ ProductService/TestClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestClient.ProductService;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ProductsServiceClient();
+            foreach (var it in client.GetProducts())
+            {
+                Console.WriteLine(it);
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
