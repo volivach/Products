bb22d35c91f5717d0f6550f5a2301ff4961ac250
Liadov Nicolay <avtodrivevid@gmail.com>
2015-12-26 18:12:48 +0200

    WCF Product Serice add test client

---
+++ ProductService/ProductService/ProductsService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProductService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ProductsService : IProductsService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public List<string> GetProducts()
+        {
+            return new List<string>() { "LG-101","Thinc Padt440s","IPhone 6s" };
+        }
+    }
+}
